61da547a520922ea95f693a63abb6f41039f6ab5
Dima <themattertng@gmail.com>
2020-12-15 21:30:11 +0300

    four

---
+++ lesson/script.js
@@ -0,0 +1,22 @@
+var first = 1;
+let second = 2;
+const pi = 3.14;
+
+
+obj = {
+    name: "John",
+    age: 25,
+    isMarried: false
+}
+console.log(obj["name"]);
+
+var arr = ['plum', 'orange', 'apple'];
+console.log(arr[0]);
+
+
+//alert("hello");
+//confirm("are you?");
+
+var anser = +prompt("Вам есть 18?", "18");
+
+console.log( typeof(anser));
